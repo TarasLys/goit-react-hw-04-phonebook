765247ec37576b0285cc6a7d4216683f086b6d30
TarasLys <opros0609@gmail.com>
2023-12-17 09:08:46 +0200

    7

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import css from './Filter.module.css';
-
-// export class Filter extends React.Component {
-//   handleChange = event => {
-//     this.props.onChange(event.target.value);
-//   };
-
-//   render() {
-//     return (
-//       <div className={css.general}>
-//         <label htmlFor="">Find contacts by name</label>
-//         <input
-//           className={css.allInput}
-//           type="text"
-//           value={this.props.value}
-//           onChange={this.handleChange}
-//           placeholder="Пошук контактів за ім'ям"
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-
-
-//import { useState } from 'react';
 import css from './Filter.module.css';
 
 export const Filter = ({ value, onChange }) => {
